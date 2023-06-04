0d5d34e41901b3e295fb4bbf9eaae501f3a08483
Camilo Griffa <camilogriffa@protonmail.com>
2023-06-03 23:15:35 -0300

    agregando clases

--- models/classes/Modelo.java
+++ models/classes/Modelo.java
@@ -1,4 +1,33 @@
 package models.classes;
-
+import java.util.Date;
 public class Modelo {
+
+  private String modelo;
+  private Date fechaModdelo;
+  private Marca marca;
+
+  public Modelo(String modelo, Date fechaModdelo,Marca marca) {
+    super();
+    this.modelo = modelo;
+    this.fechaModdelo = fechaModdelo;
+    this.marca=marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public Date getFechaModdelo() {
+    return fechaModdelo;
+  }
+
+  public void setFechaModdelo(Date fechaModdelo) {
+    this.fechaModdelo = fechaModdelo;
+  }
+
+
 }
--- models/classes/Vehiculo.java
+++ models/classes/Vehiculo.java
@@ -1,4 +1,46 @@
 package models.classes;
 
 public class Vehiculo {
+
+  private Modelo modelo;
+  private float valor;
+  private static String patente;
+
+  public Vehiculo(Modelo modelo, float valor,String patente) {
+    super();
+    this.modelo = modelo;
+    this.valor = valor;
+    patente=patente;
+  }
+
+  public Vehiculo() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  public Modelo getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(Modelo modelo) {
+    this.modelo = modelo;
+  }
+
+  public float getValor() {
+    return valor;
+  }
+
+  public void setValor(float valor) {
+    this.valor = valor;
+  }
+
+  public static String getPatente() {
+    return patente;
+  }
+
+  public static void setPatente(String patente) {
+    patente = patente;
+  }
+
+
 }
